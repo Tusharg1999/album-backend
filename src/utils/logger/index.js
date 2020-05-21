79cf8364cc5b14ea91473ed65d81bc932134a07a
Tushaaharg1999 <tushargoyal414@gmail.com>
2020-05-21 23:29:53 +0530

    Update Logger

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -1,14 +1,15 @@
 const { isDevelopment } = require("../../config");
 
-const mode = "development"
 function debugLog(msg) {
-    console.log(msg);
+    if (isDevelopment()) {
+        console.log(msg);
+    }
 }
 
 function errorLog(msg) {
-    console.error(msg);
+    if (isDevelopment()) {
+        console.error(msg);
+    }
 }
 
-if (isDevelopment()) {
-    module.exports = { debugLog, errorLog }
-}
+module.exports = { debugLog, errorLog }

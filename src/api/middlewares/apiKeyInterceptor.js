06d1b7eb0349e22089d549ba16104b4bda212259
Tushaaharg1999 <tushargoyal414@gmail.com>
2020-05-22 16:10:02 +0530

    api key interceptor middleware

--- src/api/middlewares/apiKeyInterceptor.js
+++ src/api/middlewares/apiKeyInterceptor.js
@@ -1,8 +1,9 @@
-const httpStatus = require("../../../utils/status");
+const httpStatus = require("../../utils/status");
 
 async function apiKeyInterceptor(req, res, next) {
-    const apiKey = req.body.headers['apiKey'];
+    const apiKey = req.headers['api_key'];
     if (apiKey !== process.env.API_KEY) return res.status(httpStatus.badRequest.status).send(httpStatus.badRequest.text);
 
     next();
 }
+module.exports = { apiKeyInterceptor };
--- src/loader/expressLoader/index.js
+++ src/loader/expressLoader/index.js
@@ -4,10 +4,12 @@ const helmet = require("helmet");
 
 const Routes = require("../../api/routes");
 const { debugLog } = require("../../utils/logger");
+const { apiKeyInterceptor } = require("../../api/middlewares/apiKeyInterceptor");
 
 module.exports = async function expressLoader(app) {
     app.use(cors());
     app.use(helmet());
+    app.use(await apiKeyInterceptor)
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use("/", Routes)
